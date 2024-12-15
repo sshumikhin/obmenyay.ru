d71456e21a42751f80201e40449b1eed54cbc2c8
Alexandr Shumikhin <shumikhin03@inbox.ru>
2024-12-15 22:09:32 +0300

    UPDATE: little changes

--- src/s3_client/constants.py
+++ src/s3_client/constants.py
@@ -1,6 +1,7 @@
 from enum import StrEnum
-MAX_FILE_SIZE_BYTES = 10 * 1024 * 1024
+MAX_FILE_SIZE_BYTES = 20 * 1024 * 1024
 MAX_FILE_SIZE_MB = MAX_FILE_SIZE_BYTES / 1024 / 1024
+
 
 class AllowedImageFormats(StrEnum):
     JPEG = "image/jpeg"
