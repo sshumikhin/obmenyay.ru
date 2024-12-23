6e26da8869b4126983e6109c6040d4a915fa055e
Alexandr Shumikhin <shumikhin03@inbox.ru>
2024-12-23 14:14:22 +0300

    UPDATE: testing active chat

--- src/trades/chats/websocket.py
+++ src/trades/chats/websocket.py
@@ -19,151 +19,12 @@ from src.s3_client import PUBLIC_URL as S3_PUBLIC_URL
 status_db = "waiting for owner"
 
 
-@router.websocket("/ws/{trade_id}")
-async def websocket_endpoint(websocket: WebSocket,
-                             trade_id: int,
-                             session: AsyncSession = Depends(async_session)):
-
-    # TODO: проверка на то, является ли сейчас trade активным, потому что его могли удалить
-
-
-    await websocket.accept()
-
-    user = None
-
-    try:
-        user = await vk_id.get_user_public_info(
-            access_token=websocket.cookies.get(str(JWTTokens.ACCESS.value))
-        )
-    except Exception as _:
-        await websocket.close()
-    finally:
-        if isinstance(user, vk_id.Error) or user is None:
-            await websocket.close()
-            return
-
-    async def get_user_items(session: AsyncSession):
-        user_items_ids = await get_entity_by_params(
-            session=session,
-            model=Item.id,
-            conditions=[
-                Item.owner_id == int(user.user_id)
-            ],
-            many=True
-        )
-
-        trade = await get_entity_by_params(
-            session=session,
-            model=ItemTrade,
-            conditions=[
-                and_(
-                    or_(
-                        ItemTrade.item_requested_id.in_(user_items_ids),
-                        ItemTrade.offered_by_user_id == int(user.user_id),
-                ),
-                        ItemTrade.id == trade_id
-                )
-            ],
-            load_relationships=[
-                ItemTrade.item_requested,
-                ItemTrade.interested_by_owner_item,
-                ItemTrade.interested_user
-            ],
-        )
-
-        if trade is None:
-            await websocket.close()
-            return
-
-        initial_payload = {"type": "initial_data"}
-
-        if trade.interested_item_id is None and trade.item_requested.owner_id == int(user.user_id):
-            initial_payload["status"] = "choose item"
-
-            items = await get_entity_by_params(
-                model=Item,
-                session=session,
-                conditions=[
-                    Item.owner_id == trade.offered_by_user_id
-                ],
-                many=True
-            )
-
-            content = []
-            for item in items:
-                content.append({
-                    "id": item.id,
-                    "name": item.name,
-                    "image": f"{S3_PUBLIC_URL}/{item.s3_url_path}"
-                })
-            initial_payload["items"] = content
-
-        elif trade.interested_item_id is None and trade.item_requested.owner_id != int(user.user_id):
-            initial_payload["status"] = "waiting"
-        elif trade.interested_item_id is not None and trade.is_matched is False and trade.item_requested.owner_id == int(user.user_id):
-            initial_payload["status"] = "waiting"
-        elif trade.interested_item_id is not None and trade.is_matched is False and trade.item_requested.owner_id != int(user.user_id):
-
-            initial_payload["status"] = "final answer"
-
-            second_item = await get_entity_by_params(
-                model=Item,
-                session=session,
-                conditions=[Item.id == trade.interested_item_id]
-            )
-
-            initial_payload["items"] = [{
-                "id": second_item.id,
-                "name": second_item.name,
-                "image": f"{S3_PUBLIC_URL}/{second_item.s3_url_path}"
-            }]
-        else:
-            initial_payload["status"] = "active"
-
-        await websocket.send_json(initial_payload)
-
-
-    try:
-        while True:
-            await get_user_items(session)
-            await asyncio.sleep(5)
-            # data = await websocket.receive_json()
-            # if data.get("type") == "get_all_data":
-            #     await websocket.send_json({
-            #         "type": "initial_data",
-            #         "status": status_db,
-            #         "items": items_db
-            #     })
-            #
-            #     await asyncio.sleep(4)
-            #
-            #     await websocket.send_json({
-            #         "type": "status",
-            #         "status": status_db,
-            #         "items": items_db
-            #     })
-            # elif data.get("type") == "update_status":
-            #     status_db = data.get("status")
-            #     await websocket.send_json({
-            #         "type": "status",
-            #         "status": status_db
-            #     })
-            # else:
-            #     await websocket.send_json({"type": "error", "message": "Unknown request type"})
-
-    except WebSocketDisconnect:
-        print("Client disconnected")
-    except json.JSONDecodeError:
-        print("Invalid JSON received")
-
-
-
-
-# @router.websocket("/ws/")
-# async def websocket_endpoint(
-#         websocket: WebSocket,
-#         session: AsyncSession = Depends(async_session)
-# ):
+# @router.websocket("/ws/{trade_id}")
+# async def websocket_endpoint(websocket: WebSocket,
+#                              trade_id: int,
+#                              session: AsyncSession = Depends(async_session)):
+#
+#     # TODO: проверка на то, является ли сейчас trade активным, потому что его могли удалить
 #
 #     await websocket.accept()
 #
@@ -175,36 +36,115 @@ async def websocket_endpoint(websocket: WebSocket,
 #         )
 #     except Exception as _:
 #         await websocket.close()
-#
 #     finally:
 #         if isinstance(user, vk_id.Error) or user is None:
 #             await websocket.close()
+#             return
 #
-#     active_connections.append(websocket)
+#     async def get_user_items(session: AsyncSession):
+#         user_items_ids = await get_entity_by_params(
+#             session=session,
+#             model=Item.id,
+#             conditions=[
+#                 Item.owner_id == int(user.user_id)
+#             ],
+#             many=True
+#         )
+#
+#         trade = await get_entity_by_params(
+#             session=session,
+#             model=ItemTrade,
+#             conditions=[
+#                 and_(
+#                     or_(
+#                         ItemTrade.item_requested_id.in_(user_items_ids),
+#                         ItemTrade.offered_by_user_id == int(user.user_id),
+#                 ),
+#                         ItemTrade.id == trade_id
+#                 )
+#             ],
+#             load_relationships=[
+#                 ItemTrade.item_requested,
+#                 ItemTrade.interested_by_owner_item,
+#                 ItemTrade.interested_user
+#             ],
+#         )
+#
+#         if trade is None:
+#             await websocket.close()
+#             return
+#
+#         initial_payload = {"type": "initial_data"}
+#
+#         if trade.interested_item_id is None and trade.item_requested.owner_id == int(user.user_id):
+#             initial_payload["status"] = "choose item"
+#
+#             items = await get_entity_by_params(
+#                 model=Item,
+#                 session=session,
+#                 conditions=[
+#                     Item.owner_id == trade.offered_by_user_id
+#                 ],
+#                 many=True
+#             )
+#
+#             content = []
+#             for item in items:
+#                 content.append({
+#                     "id": item.id,
+#                     "name": item.name,
+#                     "image": f"{S3_PUBLIC_URL}/{item.s3_url_path}"
+#                 })
+#             initial_payload["items"] = content
+#
+#         elif trade.interested_item_id is None and trade.item_requested.owner_id != int(user.user_id):
+#             initial_payload["status"] = "waiting"
+#         elif trade.interested_item_id is not None and trade.is_matched is False and trade.item_requested.owner_id == int(user.user_id):
+#             initial_payload["status"] = "waiting"
+#         elif trade.interested_item_id is not None and trade.is_matched is False and trade.item_requested.owner_id != int(user.user_id):
+#
+#             initial_payload["status"] = "final answer"
+#
+#             second_item = await get_entity_by_params(
+#                 model=Item,
+#                 session=session,
+#                 conditions=[Item.id == trade.interested_item_id]
+#             )
+#
+#             initial_payload["items"] = [{
+#                 "id": second_item.id,
+#                 "name": second_item.name,
+#                 "image": f"{S3_PUBLIC_URL}/{second_item.s3_url_path}"
+#             }]
+#         else:
+#             initial_payload["type"] = "active"
+#
+#         await websocket.send_json(initial_payload)
+#
 #
 #     try:
-#         await send_initial_chats(websocket, session=session, user_id=int(user.id))
-#
 #         while True:
-#             data = await websocket.receive_json()
-#             if data.get("type") == "get_chats":
-#                 await send_initial_chats(websocket)
-#             elif data.get("type") == "send_message":
-#                 chat_id = data.get("chat_id")
-#                 message = data.get("message")
-#                 if chat_id and message:
-#                     chats_data[chat_id]["last_message"] = message
-#                     for connection in active_connections:
-#                         await connection.send_text(json.dumps({
-#                             "type": "new_message",
-#                             "chat_id": chat_id,
-#                             "message": message
-#                         }))
-#             await asyncio.sleep(1)
+#             await get_user_items(session)
+#             await asyncio.sleep(5)
 #     except WebSocketDisconnect:
-#         active_connections.remove(websocket)
 #         print("Client disconnected")
-#     except RuntimeError as e:
-#         print(f"RuntimeError: {e}")
-#     except Exception as e:
-#         print(f"Unexpected error: {e}")
+#     except json.JSONDecodeError:
+#         print("Invalid JSON received")
+
+
+@router.websocket("/ws/{trade_id}")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    await websocket.send_json({"type": "active"})
+
+    while True:
+        await websocket.send_json(
+            {
+                "type": "message",
+                "text": "Hi there!",
+                "is_my": False,
+                "sender_image_url": "https://avatars.mds.yandex.net/i?id=561543dbc4c04e48e3dfecb21134d955_l-4299853-images-thumbs&n=13"
+            }
+        )
+        await asyncio.sleep(5)
+
